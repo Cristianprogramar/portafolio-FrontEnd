4e8670d5ed77dd4dafdccf821cc8a5b0e354d2b3
Cristianprogramar <111073514+Cristianprogramar@users.noreply.github.com>
2023-04-19 22:42:59 -0300

    Creación de todos los módulos
    Cree todos los módulos que voy a usar a lo largo del proyecto. Quizás me falté alguno, pero lo agregaré más adelante.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { MobileBarComponent } from './components/mobile-bar/mobile-bar.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { BannerComponent } from './components/home/banner/banner.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { SkillsComponent } from './components/home/skills/skills.component';
+import { ProjectsComponent } from './components/home/projects/projects.component';
+import { ExperienceComponent } from './components/home/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    MobileBarComponent,
+    AboutComponent,
+    BannerComponent,
+    ContactComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
